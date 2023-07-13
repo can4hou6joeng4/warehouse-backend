6479f5222aa3cc88c9bba64c236bd27c7569a909
bobochang <m13726750698_2@163.com>
2023-07-13 16:29:48 +0800

    feat: 添加统计查询模块相关类

---
+++ src/main/java/com/bobochang/warehouse/controller/StatisticsController.java
@@ -0,0 +1,31 @@
+package com.bobochang.warehouse.controller;
+
+
+import com.bobochang.warehouse.entity.Result;
+import com.bobochang.warehouse.entity.Statistics;
+import com.bobochang.warehouse.service.StatisticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/statistics")
+@RestController
+public class StatisticsController {
+
+    //注入StatisticsService
+    @Autowired
+    private StatisticsService statisticsService;
+
+    /**
+     * 统计各个仓库商品库存数量的url接口/statistics/store-invent
+     */
+    @RequestMapping("/store-invent")
+    public Result statisticsStoreInvent(){
+        //执行业务
+        List<Statistics> statisticsList = statisticsService.statisticsStoreInvent();
+        //响应
+        return Result.ok(statisticsList);
+    }
+}
