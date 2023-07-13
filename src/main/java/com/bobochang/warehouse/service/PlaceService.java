b2e33e1baa4c1c3fa7b4ea74f70104a8af69829c
bobochang <m13726750698_2@163.com>
2023-07-13 13:45:48 +0800

    feat: 添加产地模块相关类

---
+++ src/main/java/com/bobochang/warehouse/service/PlaceService.java
@@ -0,0 +1,12 @@
+package com.bobochang.warehouse.service;
+
+
+import com.bobochang.warehouse.entity.Place;
+
+import java.util.List;
+
+public interface PlaceService {
+
+    //查询所有产地的业务方法
+    public List<Place> queryAllPlace();
+}
