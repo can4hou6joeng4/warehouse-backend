d8bbccd05b846af9f9f7d093f2563abaeae28751
HuihuaLi <1843048727@qq.com>
2023-11-10 11:43:39 +0800

    modify: summary

--- src/main/java/com/bobochang/warehouse/entity/User.java
+++ src/main/java/com/bobochang/warehouse/entity/User.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 /**
@@ -59,6 +61,7 @@ public class User implements Serializable {
     /**
      * 
      */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /**
@@ -69,6 +72,7 @@ public class User implements Serializable {
     /**
      * 
      */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     @TableField(exist = false)
